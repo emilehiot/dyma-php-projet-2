367c0e57d2ef2187cf3ec9e21a19ecb2fbb1cbca
Emile Hiot <emile@mac-mini-de-emile.home>
2023-02-04 22:24:47 +0100

    first commit

---
+++ includes/header.php
@@ -0,0 +1,8 @@
+<header>
+    <a href="/" class="logo">Dyma - Blog version 1.0</a>
+    <ul class="header-menu">
+        <li class="<?= $_SERVER['REQUEST_URI'] === '/add-article.php' ? 'active' : ''?>">
+            <a href="/add-article.php">Ecrire un article</a>
+        </li>
+    </ul>
+</header>
